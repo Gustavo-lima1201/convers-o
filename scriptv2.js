547fb603821a3599904c1f765d6a9274bdebf18d
Gustavo-lima1201 <gustavogamerths1@gmail.com>
2025-05-07 17:24:48 -0300

    adicionado funções

---
+++ scriptv2.js
@@ -0,0 +1,40 @@
+function realparadolar(valorConverter) {
+  const cotacao = 5.76
+  let valorConvertido;
+  valorConvertido = (valorConverter / cotacao).toFixed(2);
+  return "U$" + valorConvertido;
+}
+
+
+function realparaeuro(valorConverter) {
+  const cotacao = 6.52
+  let valorConvertido;
+  valorConvertido = (valorConverter / cotacao).toFixed(2);
+  return `€ ${valorConvertido}`;
+}
+
+
+function main() {
+  let valorConverter, cotacaoMoeda, valorConvertido;
+  let opcaoMoeda;
+  let simboloMoeda;
+  opcaoMoeda = prompt("Olá, Qual conversão você deseja realizar hoje? 1 - Dólar 2 - Euro");
+
+  if (opcaoMoeda != 1 && opcaoMoeda != 2) {
+    alert("Opção inválida!");
+
+  } else {
+    valorConverter = prompt("Digite o valor que deseja converter: (em R$)");
+
+    if (opcaoMoeda == 1) {
+      valorConvertido = realparadolar(valorConverter);
+
+    }else{
+      valorConvertido = realparaeuro(valorConverter);
+    }
+    
+    alert( "O valor convertido é: " + valorConvertido);
+  }
+}
+
+main();
