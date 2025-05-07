567b2081775a6fb8e7ca36207cd393b53193283d
Gustavo-lima1201 <gustavogamerths1@gmail.com>
2025-05-07 13:57:13 -0300

    conver de dolar e euro

---
+++ script.js
@@ -0,0 +1,28 @@
+let valorConverter, cotacaoMoeda, valorConvertido
+let opcaoMoeda
+let simboloMoeda
+opcaoMoeda = prompt("Olá! Qual conversão você deseja realizar hoje? 1 - Dólar 2 - Euro")
+
+if (opcaoMoeda != 1 && opcaoMoeda != 2) {
+    alert("Opção inválida!")
+}
+
+else {
+    valorConverter = prompt("Digite o valor que deseja converter: (em R$)")
+
+
+
+if (opcaoMoeda == 1) {
+    cotacaoMoeda = prompt("Digite a cotação do dolar:")
+    simboloMoeda = "US$"
+    
+}
+
+else if (opcaoMoeda == 2) {
+    cotacaoMoeda = prompt("Digite a cotação do euro:")
+    simboloMoeda = "€"
+    
+}
+valorConvertido = (valorConverter / cotacaoMoeda).toFixed(2);
+alert("O valor convertido para " + simboloMoeda + " resultou em " + simboloMoeda + valorConvertido);
+}
